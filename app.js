e764be051fcec296b979def4b3f49c8ef80bb011
Erika Foin <erika.foin@gmail.com>
2019-09-19 20:02:26 -0700

    Halfway done with JS

---
+++ app.js
@@ -0,0 +1,26 @@
+function executeMadLib() {
+
+    
+    const mammalInput = document.getElementById('mammal-input')
+    const mammalInputValue = mammalInput.value;
+    const mammalSpan = document.getElementById('mammalSpan')
+    mammalSpan.textContent = mammalInputValue;
+
+    const mammalInput = document.getElementById('noun-1-input')
+    const mammalInputValue = mammalInput.value;
+    const mammalSpan = document.getElementById('noun1Span')
+    mammalSpan.textContent = mammalInputValue; 
+
+}
+
+
+//function handleKeyPress() (
+//    const nameInput = document.getElementById('name-input');
+//   const name = nameInput.Value;
+//    const greetingInput = document.getElementById('greeting-input')
+//   const greeting = greetingInput.value;
+
+ //   const nameSpan = document.getElementById('name');
+   // nameSpan.textContent = name;
+  //  const greetingSpan = document.getElementById('greeting');
+  //  greetingSpan.textContent = greeting;
